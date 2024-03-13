a272cc7111251c273ce3a976dc3acfdfe82dd7d9
iamjunhyeong <iamjunhyeong@github.com>
2024-03-13 21:09:15 +0900

    31강 ObjectMapping

---
+++ src/main/java/com/example/springboot_hongpark/objectmapper/Burger.java
@@ -0,0 +1,17 @@
+package com.example.springboot_hongpark.objectmapper;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+public class Burger {
+    private String name;
+    private int price;
+    private List<String> ingredients;
+}
